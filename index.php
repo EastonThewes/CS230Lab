673b2cf7c82dfee52492079e2af048a3c6e5de27
EastonThewes <theweseaston@gmail.com>
2021-02-02 16:42:56 -0500

    About

---
+++ index.php
@@ -0,0 +1,101 @@
+<main>
+
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+
+        <h1>About Us</h1>
+        <p class = "sellers"><b>Milk Sellers</b></p>
+
+    </div>
+
+    <div class="row">
+
+        <div class="column">
+            <div class="card">
+
+                <img src="images/1.jpeg">
+
+                <div class="container">
+
+                    <h2>CEO</h2>
+                    <p>Betsy</p>
+                    <p>Email: betsy@email.com</p>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+
+                <img src="images/2.jpg">
+
+                <div class="container">
+
+                    <h2>CMO</h2>
+                    <p>Annabelle</p>
+                    <p>Email: moo2@email.com</p>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+
+                <img src="images/3.jpg">
+
+                <div class="container">
+
+                    <h2>COO</h2>
+                    <p>Otis</p>
+                    <p>Email: barnyardOtis@email.com</p>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="column">
+            <div class="card">
+
+                <img src="images/4.jpeg">
+
+                <div class="container">
+
+                    <h2>CTO</h2>
+                    <p>John <i>"Milkman"</i> Heffer</p>
+                    <p>Email: milkman@email.com</p>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class = "row">
+        <div class = "mission">
+            <h1>Our Mission</h1>
+            <p><i>To produce the best milk</i></p>
+                <div class = "button">
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="button">
+                Store Page
+                </a>
+                </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</main>
